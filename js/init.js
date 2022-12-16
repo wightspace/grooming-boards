31069e73e864f702c3857cf11bb4bae35ae0746d
wightspace <jwight.space@gmail.com>
2022-12-16 16:30:56 -0600

    Use dev domain

--- js/init.js
+++ js/init.js
@@ -35,7 +35,7 @@ const stgURL = 'https://skisilverstar.powdr-staging.com/api/v1/dor/status';
 const prdURL = 'https://skisilverstar.com/api/v1/dor/status';
 // const url = 'data/status.json';
 
-axios.get(prdURL, {
+axios.get(devURL, {
     crossdomain: true,
     name: "data"
 }).then(function (response) {
